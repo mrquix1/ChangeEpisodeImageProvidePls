6e34f018bcc951ed00005b3abc9d24d3a9e5d103
mrquix1 <popacatalin6104@gmail.com>
2026-07-25 09:32:12 +0200

    Enhance logging in init function for TMDb

--- tmdb-episode-provider.ts
+++ tmdb-episode-provider.ts
@@ -1,7 +1,13 @@
 function init() {
     $app.onAnimeMetadataRequested((e) => {
-        console.log("[TMDb] Full mappings:", JSON.stringify(e.animeMetadata.mappings))
-        console.log("[TMDb] Full metadata keys:", Object.keys(e.animeMetadata))
+        console.log("[TMDb] Hook fired")
+        
+        try {
+            const mappings = e.animeMetadata.getMappings()
+            console.log("[TMDb] Mappings from method:", JSON.stringify(mappings))
+        } catch (err) {
+            console.error("[TMDb] Error calling getMappings:", err)
+        }
         
         e.next()
     })
